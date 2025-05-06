216cd9cee155df0e76c094480963587e5b211ee8
Davide <tdavide@posteo.net>
2025-05-06 15:22:03 +0200

    close #7: Implementata la classe convertitore

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,8 +6,24 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
     public static String convert(int number) {
-        // TODO
-        return null;
+        if (number <= 0 || number > 1000) {
+            return null;
+        }
+        
+        StringBuilder roman = new StringBuilder();
+        int remaining = number;
+        
+        for (int i = 0; i < VALUES.length; i++) {
+            while (remaining >= VALUES[i]) {
+                roman.append(SYMBOLS[i]);
+                remaining -= VALUES[i];
+            }
+        }
+        
+        return roman.toString();
     }
 }
