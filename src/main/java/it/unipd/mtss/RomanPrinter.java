f9edf28155e98d46f9a4ab5b6d825f836fba5898
unluckypippo <filippoguerra1310@gmail.com>
2025-05-06 17:56:23 +0200

    close #8: implementazione del metodo printAsciiArt

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -9,8 +9,110 @@ public class RomanPrinter {
   public static String print(int num){
   return printAsciiArt(IntegerToRoman.convert(num));
 }
-  private static String printAsciiArt(String romanNumber){
-  //TODO
-  return null;
+  private static final String[] I = {
+    " _____ ",
+    "|_   _|",
+    "  | |  ",
+    "  | |  ",
+    " _| |_ ",
+    "|_____|"
+  };
+
+  private static final String[] V = {
+    "__      __",
+    "\ \    / /",
+    " \ \  / / ",
+    "  \ \/ /  ",
+    "   \  /   ",
+    "    \/    "
+  };
+  private static final String[] X = {
+    "__    __",
+    "\ \  / /",
+    " \ V  / ",
+    "  > <   ",
+    " / . \  ",
+    "/_/ \_\ "
+  };
+  private static final String[] L = {
+    " _      ",
+    "| |     ",
+    "| |     ",
+    "| |     ",
+    "| |____ ",
+    "|______|"
+  };
+  private static final String[] C = {
+    "  _____ ",
+    " / ____|",
+    "| |     ",
+    "| |     ",
+    "| |____ ",
+    " \_____|"
+  };
+  private static final String[] D = {
+    " _____  ",
+    "|  __ \ ",
+    "| |  | |",
+    "| |  | |",
+    "| |__| |",
+    "|_____/ "
+  };
+  private static final String[] M = {
+    " __  __ ",
+    "|  \/  |",
+    "| \  / |",
+    "| |\/| |",
+    "| |  | |",
+    "|_|  |_|"
+  };
+  
+  public static String printAsciiArt(String romanNumber) {
+      if (romanNumber == null) {
+          throw new IllegalArgumentException("Input cannot be null");
+      }
+
+      if (romanNumber.isEmpty()) {
+          return "";
+      }
+
+      StringBuilder[] lines = new StringBuilder[6];
+      for (int i = 0; i < 6; i++) {
+          lines[i] = new StringBuilder();
+      }
+
+      for (char c : romanNumber.toCharArray()) {
+          String[] letterArt = getLetterArt(c);
+          for (int i = 0; i < 6; i++) {
+              if (lines[i].length() > 0) {
+                  lines[i].append(" ");
+              }
+              lines[i].append(letterArt[i]);
+          }
+      }
+
+      StringBuilder result = new StringBuilder();
+      for (int i = 0; i < 6; i++) {
+          if (i > 0) {
+              result.append("\n");
+          }
+          result.append(lines[i]);
+      }
+
+      return result.toString();
+  }
+
+  private static String[] getLetterArt(char c) {
+      switch (c) {
+          case 'I': return I;
+          case 'V': return V;
+          case 'X': return X;
+          case 'L': return L;
+          case 'C': return C;
+          case 'D': return D;
+          case 'M': return M;
+          default:
+              throw new IllegalArgumentException("Invalid Roman numeral: " + c);
+      }
   }
 }
