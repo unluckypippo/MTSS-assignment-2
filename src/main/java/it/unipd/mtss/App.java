379a639476f449911e27921fbcebb0c4bb6c5cf1
Davide <tdavide@posteo.net>
2025-05-07 10:53:04 +0200

    close #12: Implementato codice per App

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -5,14 +5,18 @@
 
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        if (args == null || args.length != 1) {
+            System.err.println("Usage: java -jar roman-number-1.0-SNAPSHOT.jar <number>");
+            return;
+        }
+
+        try {
+            int num = Integer.parseInt(args[0]);
+            System.out.println(RomanPrinter.print(num));
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
